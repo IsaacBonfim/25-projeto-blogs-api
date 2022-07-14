634348820776e7c6326ed8d65a858e53a8504148
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-13 22:34:22 -0300

    Criado o arquivo de model user.js

---
+++ src/database/models/user.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+
+const attributes = {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  displayName: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+  image: {
+    type: DataTypes.STRING,
+  },
+};
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', attributes, {
+    timestamps: false,
+    tableName: 'Users',
+  });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'posts'
+    });
+  };
+
+  return User;
+};
