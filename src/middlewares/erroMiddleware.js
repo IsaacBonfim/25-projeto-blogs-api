37d6dfdc0dafe5dcafdf4934656c76640a076941
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-15 21:10:23 -0300

    feat: criado o arquivo erroMiddleware.js

---
+++ src/middlewares/erroMiddleware.js
@@ -0,0 +1,8 @@
+module.exports = (err, _req, res, _next) => {
+  if (err.statusCode) {
+    return res.status(err.statusCode)
+      .json({ message: err.message });
+  }
+
+  res.status(500).json({ message: err.message });
+};
