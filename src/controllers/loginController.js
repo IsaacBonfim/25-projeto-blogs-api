a9c2a843d40ace88d5ed830813dd184d3bb00cfd
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-15 21:45:37 -0300

    fix: ajuste no arquivo loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,12 +1,12 @@
 const service = require('../services/loginService');
-const tokenFc = require('../helpers/tokenFunctions');
+const tokenMaker = require('../helpers/tokenFunctions');
 
 module.exports = {
   login: async (req, res) => {
     const { email, password } = req.body;
-    const validate = await service.loginValidation({ email, password });
-    const user = await service.userValidation(validate);
-    const token = await tokenFc.tokenMaker(user);
+    const validated = await service.loginValidation({ email, password });
+    const user = await service.userValidation(validated);
+    const token = await tokenMaker(user);
 
     res.status(200).json({ token });
   },
