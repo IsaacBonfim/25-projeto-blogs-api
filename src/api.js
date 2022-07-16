9aa56e5285cc0cee7852419c7c234f922942f437
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-15 21:17:03 -0300

    Enviando requisito 3

--- src/api.js
+++ src/api.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
-// ...
+require('express-async-errors');
+
+const login = require('./routes/loginRoute');
+const error = require('./middlewares/erroMiddleware');
 
 const app = express();
 
 app.use(express.json());
 
-// ...
+app.use('/login', login);
+
+app.use(error);
 
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
