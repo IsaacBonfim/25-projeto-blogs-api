7345d7cd45122a971b639e3f4331d0e9cafe11f7
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-16 00:54:40 -0300

    Enviando requisito 5

--- src/middlewares/erroMiddleware.js
+++ src/middlewares/erroMiddleware.js
@@ -4,5 +4,10 @@ module.exports = (err, _req, res, _next) => {
       .json({ message: err.message });
   }
 
+  if (err.name === 'JsonWebTokenError') {
+    return res.status(401)
+      .json({ message: 'Expired or invalid token' });
+  }
+
   res.status(500).json({ message: err.message });
 };
