d3780106da455b609d4d8437999cf5c7033f2059
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-16 00:47:04 -0300

    feat: Adição da rota GET no arquivo userRoute.js

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,8 +1,11 @@
 const { Router } = require('express');
 const controller = require('../controllers/userController');
+const tokenMd = require('../middlewares/tokenMiddleware');
 
 const user = Router();
 
-user.route('/').post(controller.userCreation);
+user.route('/')
+  .post(controller.userCreation)
+  .get(tokenMd, controller.getAllUsers);
 
 module.exports = user;
